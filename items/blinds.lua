9c4572603c4e7575f9e5a1824ac21569a75d8e4c
Ruby <138878489+lord-ruby@users.noreply.github.com>
2025-04-22 21:43:53 +0100

    Update blinds.lua

--- items/blinds.lua
+++ items/blinds.lua
@@ -12,3 +12,178 @@ SMODS.Blind({
 		showdown = true,
 	}
 })
+
+SMODS.Blind({
+	key = "burgundy_baracuda",
+	pos = { x = 0, y = 2 },
+	atlas = "blinds",
+	boss_colour = HEX("FF0000"),
+    mult=3,
+    dollars = 8,
+	boss = {
+		min = 3,
+		max = 10,
+		showdown = true,
+	},
+	calculate = function(self, blind, context)
+			if
+				context.destroy_card
+				and (context.cardarea == G.play or context.cardarea == "unscored")
+				and not G.GAME.blind.disabled
+				and pseudorandom("baracuda") < 0.5
+			then
+				return {remove=true}
+			end
+	end
+})
+
+SMODS.Blind({
+	key = "diamond_dawn",
+	pos = { x = 0, y = 3 },
+	atlas = "blinds",
+	boss_colour = HEX("00d5ff"),
+    mult=3,
+    dollars = 8,
+	boss = {
+		min = 3,
+		max = 10,
+		showdown = true,
+	},
+	config = {
+		extra = {
+			hand_size = -2
+		}
+	},
+	calculate = function(self, blind, context)
+		if context.pre_discard and not G.GAME.blind.disabled then
+			Entropy.FlipThen(G.hand.highlighted, function(card, area)
+				SMODS.change_base(card, "entr_nilsuit", "entr_nilrank")
+			end)
+		end
+	end,
+	set_blind = function(self)
+        if not G.GAME.blind.disabled then G.hand:change_size(self.config.extra.hand_size) end
+	end,
+	defeat = function(self)
+        if not G.GAME.blind.disabled then G.hand:change_size(-self.config.extra.hand_size) end
+    end,
+    disable = function(self)
+        G.hand:change_size(-self.config.extra.hand_size)
+        G.FUNCS.draw_from_deck_to_hand(-self.config.extra.hand_size)
+    end
+})
+
+SMODS.Blind({
+	key = "olive_orchard",
+	pos = { x = 0, y = 4 },
+	atlas = "blinds",
+	boss_colour = HEX("55ae29"),
+    mult=3,
+    dollars = 8,
+	boss = {
+		min = 3,
+		max = 10,
+		showdown = true,
+	},
+	calculate = function(self, blind, context)
+		if context.pre_discard and not G.GAME.blind.disabled then
+			Entropy.FlipThen(G.hand.highlighted, function(card, area)
+				card:set_ability(G.P_CENTERS.m_entr_disavowed)
+			end)
+		end
+		if context.final_scoring_step and not G.GAME.blind.disabled then
+			Entropy.FlipThen(G.play.cards, function(card, area)
+				card:set_ability(G.P_CENTERS.m_entr_disavowed)
+			end)
+		end
+	end,
+})
+
+SMODS.Blind({
+	key = "citrine_comet",
+	pos = { x = 0, y = 5 },
+	atlas = "blinds",
+	boss_colour = HEX("ffee00"),
+    mult=3,
+    dollars = 8,
+	boss = {
+		min = 3,
+		max = 10,
+		showdown = true,
+	},
+	calculate = function(self, blind, context)
+		if context.check then 
+			for i, v in pairs(G.hand.cards) do if not v.highlighted then v.destroy_adjacent = false;v.destroyed_adjacent = false end end
+			for i, v in pairs(G.hand.highlighted) do
+				v.destroy_adjacent = true
+			end
+			for i, v in pairs(G.hand.cards) do
+				if v.destroy_adjacent and not v.destroyed_adjacent then
+					if G.hand.cards[i-1] then
+						G.hand.cards[i-1]:start_dissolve()
+						G.hand.cards[i-1].ability.temporary2 = true
+					end
+					if G.hand.cards[i+1] then
+						G.hand.cards[i+1]:start_dissolve()
+						G.hand.cards[i+1].ability.temporary2 = true
+					end
+					v.destroy_adjacent = false
+					v.destroyed_adjacent = true
+				end
+			end
+		end
+	end,
+})
+Entropy.EEBlacklist = {
+	bl_cry_obsidian_orb=true,
+	bl_entr_endless_entropy=true
+}
+SMODS.Blind({
+	key = "endless_entropy",
+	pos = { x = 0, y = 6 },
+	atlas = "blinds",
+	boss_colour = HEX("ff00ff"),
+    mult=1,
+	no_ee = true,
+	exponent = {
+		2, 3
+	},
+    dollars = 8,
+	boss = {
+		min = 32,
+		max = 32,
+		showdown = true,
+	},
+	counts_as = {
+		["bl_entr_scarlet_sun"]=true
+	},
+	calculate = function(self, blind, context)
+		for i, v in pairs(G.P_BLINDS) do
+			if not Entropy.EEBlacklist[i] and v.boss and v.boss.showdown and v.calculate and not v.no_ee then
+				v:calculate(blind, context)
+			end
+		end
+	end,
+	set_blind = function(self)
+		for i, v in pairs(G.P_BLINDS) do
+			if not Entropy.EEBlacklist[i] and v.boss and v.boss.showdown and v.set_blind and not v.no_ee then
+				v:set_blind()
+			end
+		end
+		G.GAME.blind.chips = to_big(G.GAME.blind.chips):arrow(self.exponent[1],self.exponent[2])
+	end,
+	defeat = function(self)
+		for i, v in pairs(G.P_BLINDS) do
+			if not Entropy.EEBlacklist[i] and v.boss and v.boss.showdown and v.defeat and not v.no_ee then
+				v:defeat()
+			end
+		end
+    end,
+    disable = function(self)
+		for i, v in pairs(G.P_BLINDS) do
+			if not Entropy.EEBlacklist[i] and v.boss and v.boss.showdown and v.disable and not v.no_ee then
+				v:disable()
+			end
+		end
+    end
+})
