a873cb409a4ece3e12b5fa79d55d4fcd413db093
Ruby <138878489+lord-ruby@users.noreply.github.com>
2025-04-29 22:20:56 +0100

    temporarily remove crimson seals effect

--- items/misc/seals.lua
+++ items/misc/seals.lua
@@ -15,149 +15,149 @@ local crimson = {
     end,
 }
 
-function SMODS.calculate_main_scoring(context, scoring_hand)
-    for _, card in ipairs(context.cardarea.cards) do
-        if (card.seal == "entr_crimson" and not G.GAME.crimson_seal) or (card.seal ~= "entr_crimson" and G.GAME.crimson_seal) then return end
-            local in_scoring = scoring_hand and SMODS.in_scoring(card, context.scoring_hand)
-            --add cards played to list
-            if scoring_hand and not SMODS.has_no_rank(card) and in_scoring then
-                G.GAME.cards_played[card.base.value].total = G.GAME.cards_played[card.base.value].total + 1
-                if not SMODS.has_no_suit(card) then
-                    G.GAME.cards_played[card.base.value].suits[card.base.suit] = true
-                end
-            end
-            --if card is debuffed
-            if scoring_hand and card.debuff then
-                if in_scoring then 
-                    G.GAME.blind.triggered = true
-                    G.E_MANAGER:add_event(Event({
-                        trigger = 'immediate',
-                        func = (function() SMODS.juice_up_blind();return true end)
-                    }))
-                    card_eval_status_text(card, 'debuff')
-                end
-            else
-                if scoring_hand then
-                    if in_scoring then context.cardarea = G.play else context.cardarea = 'unscored' end
-                end
-                if card.seal == "entr_crimson" then
-                    local text, loc_disp_text, poker_hands, scoring_hand, disp_text =
-                    G.FUNCS.get_poker_hand_info(G.play.cards)
-                    print(SMODS.in_scoring(card, scoring_hand))
-                    if SMODS.in_scoring(card, scoring_hand) then
-                        local delay = 0.6
-                        SMODS.score_card(card, context)
-                        Entropy.Again(card)
-                        SMODS.score_card(card, context)
-                        Entropy.Again(card)
-                        SMODS.score_card(card, context)
-                    end
-                else
-                    SMODS.score_card(card, context)
-                end
-            end
-    end
-end
-function SMODS.score_card(card, context)
-    local reps = { 1 }
-    local j = 1
-    while j <= #reps do
-        if reps[j] ~= 1 then
-            local _, eff = next(reps[j])
-            SMODS.calculate_effect(eff, eff.card)
-            percent = percent + (percent_delta or 0.08)
-        end
+-- function SMODS.calculate_main_scoring(context, scoring_hand)
+--     for _, card in ipairs(context.cardarea.cards) do
+--         if (card.seal == "entr_crimson" and not G.GAME.crimson_seal) or (card.seal ~= "entr_crimson" and G.GAME.crimson_seal) then return end
+--             local in_scoring = scoring_hand and SMODS.in_scoring(card, context.scoring_hand)
+--             --add cards played to list
+--             if scoring_hand and not SMODS.has_no_rank(card) and in_scoring then
+--                 G.GAME.cards_played[card.base.value].total = G.GAME.cards_played[card.base.value].total + 1
+--                 if not SMODS.has_no_suit(card) then
+--                     G.GAME.cards_played[card.base.value].suits[card.base.suit] = true
+--                 end
+--             end
+--             --if card is debuffed
+--             if scoring_hand and card.debuff then
+--                 if in_scoring then 
+--                     G.GAME.blind.triggered = true
+--                     G.E_MANAGER:add_event(Event({
+--                         trigger = 'immediate',
+--                         func = (function() SMODS.juice_up_blind();return true end)
+--                     }))
+--                     card_eval_status_text(card, 'debuff')
+--                 end
+--             else
+--                 if scoring_hand then
+--                     if in_scoring then context.cardarea = G.play else context.cardarea = 'unscored' end
+--                 end
+--                 if card.seal == "entr_crimson" then
+--                     local text, loc_disp_text, poker_hands, scoring_hand, disp_text =
+--                     G.FUNCS.get_poker_hand_info(G.play.cards)
+--                     print(SMODS.in_scoring(card, scoring_hand))
+--                     if SMODS.in_scoring(card, scoring_hand) then
+--                         local delay = 0.6
+--                         SMODS.score_card(card, context)
+--                         Entropy.Again(card)
+--                         SMODS.score_card(card, context)
+--                         Entropy.Again(card)
+--                         SMODS.score_card(card, context)
+--                     end
+--                 else
+--                     SMODS.score_card(card, context)
+--                 end
+--             end
+--     end
+-- end
+-- function SMODS.score_card(card, context)
+--     local reps = { 1 }
+--     local j = 1
+--     while j <= #reps do
+--         if reps[j] ~= 1 then
+--             local _, eff = next(reps[j])
+--             SMODS.calculate_effect(eff, eff.card)
+--             percent = percent + (percent_delta or 0.08)
+--         end
 
-        context.main_scoring = true
-        local effects = { eval_card(card, context) }
-        SMODS.calculate_quantum_enhancements(card, effects, context)
-        context.main_scoring = nil
-        context.individual = true
-        context.other_card = card
+--         context.main_scoring = true
+--         local effects = { eval_card(card, context) }
+--         SMODS.calculate_quantum_enhancements(card, effects, context)
+--         context.main_scoring = nil
+--         context.individual = true
+--         context.other_card = card
 
-        if next(effects) then
-            SMODS.calculate_card_areas('jokers', context, effects, { main_scoring = true })
-            SMODS.calculate_card_areas('individual', context, effects, { main_scoring = true })
-        end
+--         if next(effects) then
+--             SMODS.calculate_card_areas('jokers', context, effects, { main_scoring = true })
+--             SMODS.calculate_card_areas('individual', context, effects, { main_scoring = true })
+--         end
 
-        local flags = SMODS.trigger_effects(effects, card)
+--         local flags = SMODS.trigger_effects(effects, card)
 
-        context.individual = nil
-        if reps[j] == 1 and flags.calculated then
-            context.repetition = true
-            context.card_effects = effects
-            SMODS.calculate_repetitions(card, context, reps)
-            context.repetition = nil
-            context.card_effects = nil
-        end
-        j = j + (flags.calculated and 1 or #reps)
-        context.other_card = nil
-        card.lucky_trigger = nil
-    end
-end
+--         context.individual = nil
+--         if reps[j] == 1 and flags.calculated then
+--             context.repetition = true
+--             context.card_effects = effects
+--             SMODS.calculate_repetitions(card, context, reps)
+--             context.repetition = nil
+--             context.card_effects = nil
+--         end
+--         j = j + (flags.calculated and 1 or #reps)
+--         context.other_card = nil
+--         card.lucky_trigger = nil
+--     end
+-- end
 
-function evaluate_play_final_scoring(text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta)
-    local txt,_,_, scoring_hand2, _ = G.FUNCS.get_poker_hand_info(G.play.cards)
-    G.GAME.crimson_seal = true
-    SMODS.calculate_main_scoring({cardarea = G.play, full_hand = G.play.cards, scoring_hand = scoring_hand2, scoring_name = text, poker_hands = poker_hands}, v == G.play and scoring_hand or nil)
-    G.GAME.crimson_seal = nil
-    for i, v in pairs(G.hand.cards) do
-        if v.seal == "entr_crimson" then
-            SMODS.score_card(v, {cardarea=G.hand,main_scoring=true})
-            if Entropy.TriggersInHand(v) then Entropy.Again(v) end
-            SMODS.score_card(v, {cardarea=G.hand,main_scoring=true})
-            if Entropy.TriggersInHand(v) and #res.playing_card > 0 then Entropy.Again(v) end
-            SMODS.score_card(v, {cardarea=G.hand,main_scoring=true})
-        end
+-- function evaluate_play_final_scoring(text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta)
+--     local txt,_,_, scoring_hand2, _ = G.FUNCS.get_poker_hand_info(G.play.cards)
+--     G.GAME.crimson_seal = true
+--     SMODS.calculate_main_scoring({cardarea = G.play, full_hand = G.play.cards, scoring_hand = scoring_hand2, scoring_name = text, poker_hands = poker_hands}, v == G.play and scoring_hand or nil)
+--     G.GAME.crimson_seal = nil
+--     for i, v in pairs(G.hand.cards) do
+--         if v.seal == "entr_crimson" then
+--             SMODS.score_card(v, {cardarea=G.hand,main_scoring=true})
+--             if Entropy.TriggersInHand(v) then Entropy.Again(v) end
+--             SMODS.score_card(v, {cardarea=G.hand,main_scoring=true})
+--             if Entropy.TriggersInHand(v) and #res.playing_card > 0 then Entropy.Again(v) end
+--             SMODS.score_card(v, {cardarea=G.hand,main_scoring=true})
+--         end
         
-    end
-    G.E_MANAGER:add_event(Event({
-    	trigger = 'after',delay = 0.4,
-        func = (function()  update_hand_text({delay = 0, immediate = true}, {mult = 0, chips = 0, chip_total = G.GAME.blind.cry_cap_score and G.GAME.blind:cry_cap_score(math.floor(hand_chips*mult)) or math.floor(hand_chips*mult), level = '', handname = ''});play_sound('button', 0.9, 0.6);return true end)
-      }))
-          G.E_MANAGER:add_event(Event({
-            trigger = 'immediate',
-            func = (function() G.GAME.current_round.current_hand.cry_asc_num = 0;G.GAME.current_round.current_hand.cry_asc_num_text = '';return true end)
-          }))
-      check_and_set_high_score('hand', hand_chips*mult)
+--     end
+--     G.E_MANAGER:add_event(Event({
+--     	trigger = 'after',delay = 0.4,
+--         func = (function()  update_hand_text({delay = 0, immediate = true}, {mult = 0, chips = 0, chip_total = G.GAME.blind.cry_cap_score and G.GAME.blind:cry_cap_score(math.floor(hand_chips*mult)) or math.floor(hand_chips*mult), level = '', handname = ''});play_sound('button', 0.9, 0.6);return true end)
+--       }))
+--           G.E_MANAGER:add_event(Event({
+--             trigger = 'immediate',
+--             func = (function() G.GAME.current_round.current_hand.cry_asc_num = 0;G.GAME.current_round.current_hand.cry_asc_num_text = '';return true end)
+--           }))
+--       check_and_set_high_score('hand', hand_chips*mult)
 
-      check_for_unlock({type = 'chip_score', chips = math.floor(hand_chips*mult)})
+--       check_for_unlock({type = 'chip_score', chips = math.floor(hand_chips*mult)})
    
-    if to_big(hand_chips)*mult > to_big(0) then
-        delay(0.8)
-        G.E_MANAGER:add_event(Event({
-        trigger = 'immediate',
-        func = (function() play_sound('chips2');return true end)
-        }))
-    end
-    G.E_MANAGER:add_event(Event({
-      trigger = 'ease',
-      blocking = false,
-      ref_table = G.GAME,
-      ref_value = 'chips',
-      ease_to = G.GAME.chips + (G.GAME.blind.cry_cap_score and G.GAME.blind:cry_cap_score(math.floor(hand_chips*mult)) or math.floor(hand_chips*mult)),
-      delay =  0.5,
-      func = (function(t) return math.floor(t) end)
-    }))
-    G.E_MANAGER:add_event(Event({
-      trigger = 'ease',
-      blocking = true,
-      ref_table = G.GAME.current_round.current_hand,
-      ref_value = 'chip_total',
-      ease_to = 0,
-      delay =  0.5,
-      func = (function(t) return math.floor(t) end)
-    }))
-    G.E_MANAGER:add_event(Event({
-      trigger = 'immediate',
-      	func = (function()
-             G.GAME.current_round.current_hand.handname = '';
-             G.GAME.asc_power_hand = nil
-        return true end)
-      }))
-      delay(0.3)
-      return text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta
-end
+--     if to_big(hand_chips)*mult > to_big(0) then
+--         delay(0.8)
+--         G.E_MANAGER:add_event(Event({
+--         trigger = 'immediate',
+--         func = (function() play_sound('chips2');return true end)
+--         }))
+--     end
+--     G.E_MANAGER:add_event(Event({
+--       trigger = 'ease',
+--       blocking = false,
+--       ref_table = G.GAME,
+--       ref_value = 'chips',
+--       ease_to = G.GAME.chips + (G.GAME.blind.cry_cap_score and G.GAME.blind:cry_cap_score(math.floor(hand_chips*mult)) or math.floor(hand_chips*mult)),
+--       delay =  0.5,
+--       func = (function(t) return math.floor(t) end)
+--     }))
+--     G.E_MANAGER:add_event(Event({
+--       trigger = 'ease',
+--       blocking = true,
+--       ref_table = G.GAME.current_round.current_hand,
+--       ref_value = 'chip_total',
+--       ease_to = 0,
+--       delay =  0.5,
+--       func = (function(t) return math.floor(t) end)
+--     }))
+--     G.E_MANAGER:add_event(Event({
+--       trigger = 'immediate',
+--       	func = (function()
+--              G.GAME.current_round.current_hand.handname = '';
+--              G.GAME.asc_power_hand = nil
+--         return true end)
+--       }))
+--       delay(0.3)
+--       return text, disp_text, poker_hands, scoring_hand, non_loc_disp_text, percent, percent_delta
+-- end
 
 local sapphire = {
     dependencies = {
