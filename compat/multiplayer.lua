99607dbe799eb84f0b1a27c26ae32fa3f8f3299f
Ruby <138878489+lord-ruby@users.noreply.github.com>
2025-05-01 00:43:16 +0100

    fix typo

--- compat/multiplayer.lua
+++ compat/multiplayer.lua
@@ -3,7 +3,7 @@ return function()
 	MP.DECK.ban_card("j_entr_ruby")
     MP.DECK.ban_card("c_entr_new")
     for i, b in pairs(Entropy.BlindC) do
-        MP.DECK.ban_card("c_"..v)
+        MP.DECK.ban_card("c_"..b)
     end
     MP.DECK.ban_card("j_entr_xekanos")
 end
