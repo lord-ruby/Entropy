56cdfea61346490d85e06090970639298ac5bbc4
Ruby <138878489+lord-ruby@users.noreply.github.com>
2025-05-08 17:44:56 +0100

    oops

--- items/jokers/zenith_jokers.lua
+++ items/jokers/zenith_jokers.lua
@@ -7,6 +7,8 @@ local ruby = {
     atlas = "ruby_atlas",
     pos = {x=0, y=0},
     soul_pos = {x = 1, y = 0},
+    no_doe = true,
+    no_collection = true,
     loc_vars = function()
         return {
             vars = {
