fdbc483f1223973d4094de8a3515740e828737b9
Ruby <138878489+lord-ruby@users.noreply.github.com>
2025-05-20 17:05:50 +0100

    dont let ee be rerolled

--- lib/hooks.lua
+++ lib/hooks.lua
@@ -2370,3 +2370,11 @@ function Card:calculate_banana()
 	end
 	return false
 end
+
+local get_bossref = get_new_boss
+function get_new_boss()
+    if (G.GAME.EEBuildup or (to_big(G.GAME.round_resets.ante) >= to_big(32) and not G.GAME.EEBeaten)) and Entropy.CanEeSpawn() then
+        return "bl_entr_endless_entropy_phase_one"
+    end
+    return get_bossref()
+end
