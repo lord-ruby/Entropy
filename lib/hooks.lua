fa74108ff2fc67c67e965e05f8fb33b3ecd018fb
Ruby <138878489+lord-ruby@users.noreply.github.com>
2025-06-02 23:12:27 +0100

    fix nerf

--- lib/hooks.lua
+++ lib/hooks.lua
@@ -2930,6 +2930,8 @@ function CardArea:parse_highlighted()
     local text,disp_text,poker_hands = G.FUNCS.get_poker_hand_info(self.highlighted)
     if text == "High Card" and G.GAME.hands["High Card"].operator then
         local colours = {
+            [-1] = HEX("a26161"),
+            [0] = G.C.RED,
             [1] = G.C.EDITION,
             [2] = G.C.CRY_ASCENDANT,
             [3] = G.C.CRY_EXOTIC,
