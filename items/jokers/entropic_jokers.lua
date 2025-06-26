2c07bc7f8430fe370472be5eb897f4d28bdda78e
Ruby <darkdragongaming379@gmail.com>
2025-06-26 14:29:46 +0100

    nerf epi forcetrigger

--- items/jokers/entropic_jokers.lua
+++ items/jokers/entropic_jokers.lua
@@ -28,7 +28,7 @@ local epitachyno = {
                 local check = false
                 local exp = card.ability.extra
 			    --local card = G.jokers.cards[i]
-                if not Card.no(G.jokers.cards[i], "immutable", true) and (G.jokers.cards[i].config.center.key ~= "j_entr_epitachyno" or context.forcetrigger) then
+                if not Card.no(G.jokers.cards[i], "immutable", true) and (G.jokers.cards[i].config.center.key ~= "j_entr_epitachyno") then
                     Cryptid.manipulate(card2, { value = {arrows = 1, height = exp}, type = "hyper" })
                     check = true
                 end
