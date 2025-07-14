3878815d78d20f3194ed31f9fd11c3eb4ccaff0a
Ruby <darkdragongaming379@gmail.com>
2025-07-14 14:34:43 +0100

    update collection_rows

--- items/misc/consumable_types.lua
+++ items/misc/consumable_types.lua
@@ -23,7 +23,7 @@ SMODS.ConsumableType({
 	key = "Fraud",
 	primary_colour = G.C.Entropy.Fraud,
 	secondary_colour = HEX("a54747"),
-	collection_rows = { 4, 4 },
+	collection_rows = { 4, 5 },
 	shop_rate = 0.0,
 	loc_txt = {},
 	default = "c_entr_master"
