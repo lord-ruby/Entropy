d76cfca09de3a193f1e56333ab364da336f67c48
cassknows <kassakohl@gmail.com>
2025-07-25 00:26:43 +0200

    🌞

--- items/jokers/rlegendary_jokers.lua
+++ items/jokers/rlegendary_jokers.lua
@@ -270,7 +270,7 @@ local entropy_card = {
     key = "entropy_card",
     config = {
         x_asc_mod = 1,
-        num = 862 --sun
+        num = 913 --sun
     },
     dependencies = {
         items = {
