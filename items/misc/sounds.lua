8a0d30100b6bc04ee63c493d26b54e3f52625ab9
Ruby <darkdragongaming379@gmail.com>
2025-09-08 20:04:19 +0100

    twisted pack music

--- items/misc/sounds.lua
+++ items/misc/sounds.lua
@@ -53,3 +53,13 @@ SMODS.Sound({
 	path = "e_rizz.ogg",
 	volume = 0.7
 })
+
+SMODS.Sound({
+	key = "music_dread",
+	path = "music_dread.ogg",
+	select_music_track = function()
+    if not G.screenwipe and G.STATE == G.STATES.SMODS_BOOSTER_OPENED and SMODS.OPENED_BOOSTER and string.find(SMODS.OPENED_BOOSTER.config.center.key, "twisted", 0, true) ~= nil then
+      return 1339
+    end
+  end
+})
