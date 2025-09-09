67eef7c271563585ba71bddd18d4a62aba4abda1
Ruby <darkdragongaming379@gmail.com>
2025-09-09 16:56:19 +0100

    better checks for echo

--- lib/utils.lua
+++ lib/utils.lua
@@ -1904,3 +1904,8 @@ function Entropy.allow_spawning(center)
     end
     return true
 end
+
+function Entropy.get_reroll_height()
+    if HotPotato then return 1.04 end
+    return Entropy.CanSwitchAltPath() and 1 or 1.6
+end
