31b9cb332bd7acd63c8289e83c2aeee7cfe5c041
f-raZ0R <MonkeyCityOwner@gmail.com>
2025-09-28 10:46:16 -0400

    Implement Four out of Six bunco frauds

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -2048,6 +2048,51 @@ local decs = {
 					"and {C:attention}distribute{} their modifications",
 					"between {C:attention}other{} cards held in hand"
 				}
+			},
+			--Not too fond of this name feel free to change it
+			c_entr_rectification = {
+				name = "The Rectification",
+				text = {
+					"Banish {C:attention}#1#{} selected card#<s>1#",
+					"Without {C:attention}destroying{} them"
+				}
+			},
+			c_entr_avarice = {
+				name = "Avarice",
+				text = {
+					"Immediately open {C:attention}#1#{} Standard Packs"
+				}
+			},
+			c_entr_symphony = {
+				name = "The Symphony",
+				text = {
+					"{C:attention}Link together{} any",
+					"#1# selected cards"
+				}
+			},
+			--Description feels awkwardly worded.
+			c_entr_microcosm = {
+				name = "The Microcosm",
+				text = {
+					"{C:attention} Randomize{} the rank and suit of {C:attention}#1#{} cards",
+					"all into the same rank and suit"
+				}
+			},
+			c_entr_desert = {
+				name = "The Desert",
+				text = {
+					"{S:0.8}({S:0.8,V:1}lvl.#1#{}{S:0.8,C:gold}#2#{}{S:0.8}){} Level up",
+					"{C:attention}Fleurons",
+					"{C:blue}+#4#{} Chips"
+				}
+			},
+			c_entr_metropolis = {
+				name = "The Metropolis",
+				text = {
+					"{S:0.8}({S:0.8,V:1}lvl.#1#{}{S:0.8,C:gold}#2#{}{S:0.8}){} Level up",
+					"{C:attention}Halberds",
+					"{C:blue}+#4#{} Chips"
+				}
 			}
 		},
 		Planet = {
