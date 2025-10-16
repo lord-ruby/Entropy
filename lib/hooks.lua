4ee06ea8a48a8537d064ccf0acf28537d245bd23
Ruby <138878489+lord-ruby@users.noreply.github.com>
2025-10-16 16:27:07 +0100

    plasma and anaglyph work with redeeming

--- lib/hooks.lua
+++ lib/hooks.lua
@@ -1747,6 +1747,70 @@ function SMODS.calculate_context(context, return_table)
     end
 end
 
+local trigger_effectref = Back.trigger_effect
+function Back:trigger_effect(args, ...)
+    local ret = trigger_effectref(self, args, ...)
+    if G.GAME.entr_bought_decks then
+        for i, v in pairs(G.GAME.entr_bought_decks or {}) do
+            if v == 'b_anaglyph' and args.context == 'eval' and G.GAME.last_blind and G.GAME.last_blind.boss then
+                G.E_MANAGER:add_event(Event({
+                    func = (function()
+                        add_tag(Tag('tag_double'))
+                        play_sound('generic1', 0.9 + math.random()*0.1, 0.8)
+                        play_sound('holo1', 1.2 + math.random()*0.1, 0.4)
+                        return true
+                    end)
+                }))
+            end
+            if v == "b_plasma" and args.context == 'final_scoring_step' then
+                local tot = args.chips + args.mult
+                args.chips = math.floor(tot/2)
+                args.mult = math.floor(tot/2)
+                update_hand_text({delay = 0}, {mult = args.mult, chips = args.chips})
+
+                G.E_MANAGER:add_event(Event({
+                    func = (function()
+                        local text = localize('k_balanced')
+                        play_sound('gong', 0.94, 0.3)
+                        play_sound('gong', 0.94*1.5, 0.2)
+                        play_sound('tarot1', 1.5)
+                        ease_colour(G.C.UI_CHIPS, {0.8, 0.45, 0.85, 1})
+                        ease_colour(G.C.UI_MULT, {0.8, 0.45, 0.85, 1})
+                        attention_text({
+                            scale = 1.4, text = text, hold = 2, align = 'cm', offset = {x = 0,y = -2.7},major = G.play
+                        })
+                        G.E_MANAGER:add_event(Event({
+                            trigger = 'after',
+                            blockable = false,
+                            blocking = false,
+                            delay =  4.3,
+                            func = (function() 
+                                    ease_colour(G.C.UI_CHIPS, G.C.BLUE, 2)
+                                    ease_colour(G.C.UI_MULT, G.C.RED, 2)
+                                return true
+                            end)
+                        }))
+                        G.E_MANAGER:add_event(Event({
+                            trigger = 'after',
+                            blockable = false,
+                            blocking = false,
+                            no_delete = true,
+                            delay =  6.3,
+                            func = (function() 
+                                G.C.UI_CHIPS[1], G.C.UI_CHIPS[2], G.C.UI_CHIPS[3], G.C.UI_CHIPS[4] = G.C.BLUE[1], G.C.BLUE[2], G.C.BLUE[3], G.C.BLUE[4]
+                                G.C.UI_MULT[1], G.C.UI_MULT[2], G.C.UI_MULT[3], G.C.UI_MULT[4] = G.C.RED[1], G.C.RED[2], G.C.RED[3], G.C.RED[4]
+                                return true
+                            end)
+                        }))
+                        return true
+                    end)
+                }))
+            end
+        end
+    end
+    return ret
+end
+
 local ref = create_shop_card_ui
 function create_shop_card_ui(card, type, area)
     if card.config.center.set == "Back" or card.config.center.set == "Sleeve" then
