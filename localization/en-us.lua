55bac1494bd096a3ac9d03347a7e2295437bd270
Ruby <138878489+lord-ruby@users.noreply.github.com>
2025-10-17 21:29:59 +0100

    antithesis resprite

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -3356,7 +3356,6 @@ local decs = {
 				name = "Pact of Serpents",
 				text = {
 					"Create a random {C:attention}Rare{} Consumable",
-					"{C:money}-$#1#{}",
 					"{C:inactive}(May overflow){}"
 				}
 			},
