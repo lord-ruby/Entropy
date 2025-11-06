5cbca661580ba63eebe1be064a3c216c2d4d65ce
Ruby <138878489+lord-ruby@users.noreply.github.com>
2025-11-06 21:16:54 +0000

    kings scepter sprite

--- items/jokers/misc_jokers.lua
+++ items/jokers/misc_jokers.lua
@@ -6256,8 +6256,8 @@ local kings_scepter = {
     rarity = 2,
     cost = 6,
     eternal_compat = true,
-    pos = {x = 1, y = 0},
-    atlas = "placeholder",
+    pos = {x = 2, y = 15},
+    atlas = "jokers",
     dependencies = {
         items = {
             "set_entr_misc_jokers",
