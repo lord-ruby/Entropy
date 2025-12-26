46820fa83c3b9290947c8faa59aaf497a5b57fea
Ruby <138878489+lord-ruby@users.noreply.github.com>
2025-12-26 19:38:38 +0000

    nil check for get type color

--- items/misc/decks.lua
+++ items/misc/decks.lua
@@ -227,7 +227,7 @@ function get_type_colour(_c, card)
     if Entropy.IsEE() and card.debuff then
       return Entropy.reverse_legendary_gradient
     end
-    if card.ability and card.ability.glitched_crown and G.P_CENTERS[card.ability.glitched_crown[card.glitched_index]] then
+    if card and card.ability and card.ability.glitched_crown and G.P_CENTERS[card.ability.glitched_crown[card.glitched_index]] then
         return get_type_colourref(G.P_CENTERS[card.ability.glitched_crown[card.glitched_index]], card)
     end
     return get_type_colourref(_c, card)
