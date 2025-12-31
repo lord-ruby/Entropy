4df47b37ffd23030b24027396d4f594f77db3fb4
Ruby <138878489+lord-ruby@users.noreply.github.com>
2025-12-31 01:39:05 +0000

    fix ordering

--- items/misc/boosters.lua
+++ items/misc/boosters.lua
@@ -228,7 +228,7 @@ local voucher = {
         }
     },
 	object_type = "Booster",
-    order = -1001.1,
+    order = -990.1,
     key = "voucher_pack",
     set = "Booster",
     config = { extra = 5, choose = 2 },
